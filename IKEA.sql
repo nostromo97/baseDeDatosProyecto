10804f58214c3007ab6c0adfa3e198b7d8241704
antjrp02 <antjrp02@gmail.com>
2022-03-15 13:51:05 +0100

    Update IKEA.sql

--- IKEA.sql
+++ IKEA.sql
@@ -18,7 +18,7 @@ CREATE TABLE Trabajadores
      Apellidos       VARCHAR2 (20)  NOT NULL , 
      DNI             VARCHAR2 (9)  NOT NULL , 
      Nº_telefono     NUMBER (9)  NOT NULL , 
-     Sueldo          NUMBER (5,2)  NOT NULL , 
+     Sueldo          NUMBER (5)  NOT NULL , 
      Direccion       VARCHAR2 (20)  NOT NULL , 
      Tipo            VARCHAR2 (18)  NOT NULL , 
      Tienda_ID       NUMBER (9)  NOT NULL 
@@ -61,7 +61,7 @@ CREATE TABLE Productos
      id_producto NUMBER (8) PRIMARY KEY,
      Tipo               VARCHAR2 (12)  NOT NULL , 
      Precio             NUMBER (4)  NOT NULL , 
-     Descripción        VARCHAR2 (20)  NOT NULL , 
+     Descripción        VARCHAR2 (40)  NOT NULL , 
      Cantidad           NUMBER (9)  NOT NULL , 
      id_Departamento    NUMBER(8)  NOT NULL , 
      Linea_de_pedido_ID NUMBER (8)  NOT NULL 
@@ -81,14 +81,16 @@ CREATE TABLE Departamento
 ---------------------------------------------
 CREATE TABLE Iluminacion 
     ( 
-     id_departamento NUMBER(8)PRIMARY KEY , 
+     id_lampara NUMBER(8)PRIMARY KEY,
+     id_departamento NUMBER(8)NOT NULL , 
      Lamparas      VARCHAR2 (20) , 
      Smart         VARCHAR2 (20) 
     ) 
 ;
 CREATE TABLE Jardineria 
     ( 
-     id_departamento NUMBER(8)PRIMARY KEY , 
+     id_jardineria NUMBER(8)PRIMARY KEY,
+     id_departamento NUMBER(8)NOT NULL , 
      Macetas       VARCHAR2 (20) , 
      Plantas       VARCHAR2 (20) 
     ) 
@@ -96,14 +98,16 @@ CREATE TABLE Jardineria
 
 CREATE TABLE Muebles 
     ( 
-     id_departamento NUMBER(8)PRIMARY KEY  , 
+     id_mueble NUMBER(8)PRIMARY KEY,
+     id_departamento NUMBER(8)NOT NULL , 
      Tipo          VARCHAR2 (20) , 
      Material      VARCHAR2 (20) 
     ) 
 ;
 CREATE TABLE Bricolaje 
     ( 
-     id_departamento NUMBER(8)PRIMARY KEY , 
+     id_bricolaje NUMBER(8)PRIMARY KEY ,
+     id_departamento NUMBER(8)NOT NULL, 
      Interior      VARCHAR2 (20) , 
      Herramientas  VARCHAR2 (20) 
     ) 
@@ -111,7 +115,8 @@ CREATE TABLE Bricolaje
 ----------------------------------------------
 CREATE TABLE Venden 
     ( 
-        ID_Tienda NUMBER (8)PRIMARY KEY , 
+        id_venta NUMBER (8)PRIMARY KEY,
+        ID_Tienda NUMBER (8)NOT NULL , 
         id_producto NUMBER (8) NOT NULL
     ) 
 ;
@@ -155,12 +160,7 @@ ALTER TABLE Recepcionista ADD CONSTRAINT Recepcionista_Trabajador_fk FOREIGN KEY
 ALTER TABLE Carpinteros ADD CONSTRAINT Carpinteros_Trabajador_fk FOREIGN KEY (id) REFERENCES Trabajadores(Trabajadores_ID)ON DELETE CASCADE;
 ALTER TABLE Directivos ADD CONSTRAINT Directivos_Trabajador_fk FOREIGN KEY (id) REFERENCES Trabajadores(Trabajadores_ID)ON DELETE CASCADE;
 ALTER TABLE Director_marketing ADD CONSTRAINT Dir_mark_Trabajador_fk FOREIGN KEY (id) REFERENCES Trabajadores(Trabajadores_ID)ON DELETE CASCADE;
-ALTER TABLE Productos ADD CONSTRAINT productos_linea_FK FOREIGN KEY (Linea_de_pedido_ID) REFERENCES Linea_de_pedido(id_pedido)ON DELETE CASCADE;
 ALTER TABLE DEPARTAMENTO ADD CONSTRAINT departamento_producto_fk FOREIGN KEY (id_producto) REFERENCES Productos(id_producto)ON DELETE CASCADE;
-ALTER TABLE Iluminacion ADD CONSTRAINT iluminacion_departamento_fk FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento)ON DELETE CASCADE;
-ALTER TABLE Jardineria ADD CONSTRAINT jardineria_departamento_fk FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento)ON DELETE CASCADE;
-ALTER TABLE Muebles ADD CONSTRAINT Muebles_departamento_fk FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento)ON DELETE CASCADE;
-ALTER TABLE Bricolaje ADD CONSTRAINT Bricolaje_departamento_fk FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento)ON DELETE CASCADE;
 ALTER TABLE VENDEN ADD CONSTRAINT venden_tienda_fk FOREIGN KEY (ID_Tienda) REFERENCES Tienda(ID_Tienda) ON DELETE CASCADE;
 ALTER TABLE VENDEN ADD CONSTRAINT venden_productos_fk FOREIGN KEY (id_producto) REFERENCES Productos(id_producto)ON DELETE CASCADE;
 ALTER TABLE Pedidos ADD CONSTRAINT Pedidos_linea_fk FOREIGN KEY (Linea_de_pedido_ID ) REFERENCES Linea_de_pedido(id_pedido)ON DELETE CASCADE;
@@ -188,7 +188,7 @@ INSERT INTO Tienda VALUES(4,'Madrid','Calle 20 nº14',950,'Tarde');
 INSERT INTO Trabajadores VALUES(11,'Antonio','Rodriguez','1234578T',12356789,1200,'Calle Bolivia','Carpintero',1);
 INSERT INTO Trabajadores VALUES(33,'Emilio','Perez','44444444F',99999999,975,'Calle Brasil','Directivo',1);
 INSERT INTO Trabajadores VALUES(77,'Mario','Gonzalez','12332112W',12346365,1235,'Pura calle','Director_marketing',1);
-INSERT INTO Trabajadores VALUES(13,'Álvaro','Fernández','13288867D',C,1000,'Calle 2323','Recepcionista',1);
+INSERT INTO Trabajadores VALUES(13,'Álvaro','Fernández','13288867D',12135123,1000,'Calle 2323','Recepcionista',1);
 INSERT INTO Trabajadores VALUES(19,'Adrián','Rojas','33243527A',12135123,1000,'Calle 1543','Recepcionista',1);
 INSERT INTO Trabajadores VALUES(12,'Agustín','Arcos','65789934X',12345111,1200,'Calle Stephen','Carpintero',1);
 INSERT INTO Trabajadores VALUES(17,'Antonio','Resines','65789999F',12377711,2200,'Calle 4765','Director_marketing',1);
@@ -216,17 +216,21 @@ INSERT INTO Productos VALUES(1,'Iluminación',235,'Foco para eventos audiovisuale
 INSERT INTO Productos VALUES(2,'Jardinería',50,'Tijeras podadoras',250,2,2);
 INSERT INTO Productos VALUES(3,'Mueble',99,'Escritorio 12 piezas',300,3,3);
 INSERT INTO Productos VALUES(4,'Bricolaje',60,'Sierra de cinta',400,4,4);
-
+--DEPARTAMENTO
+INSERT INTO Departamento VALUES (1,1);
+INSERT INTO Departamento VALUES (2,2);
+INSERT INTO Departamento VALUES (3,3);
+INSERT INTO Departamento VALUES (4,4);
 --RECEPCIONISTA
-INSERT INTO Recepcionista VALUES (13,12242123);
-INSERT INTO Recepcionista VALUES(19,12135123);
-INSERT INTO Recepcionista VALUES(22,66666666);
-INSERT INTO Recepcionista VALUES(88,12020123);
+INSERT INTO Recepcionista VALUES (13,122421231);
+INSERT INTO Recepcionista VALUES (19,121351231);
+INSERT INTO Recepcionista VALUES (22,666666666);
+INSERT INTO Recepcionista VALUES (88,120201231);
 --CARPINTERO
 INSERT INTO Carpinteros VALUES(11,5900,1);
 INSERT INTO Carpinteros VALUES(12,5000,2);
-INSERT INTO Carpintero VALUES(66,3700,3);
-INSERT INTO Carpintero VALUES(99,4780,4);
+INSERT INTO Carpinteros VALUES(66,3700,3);
+INSERT INTO Carpinteros VALUES(99,4780,4);
 --DIRECTIVO
 INSERT INTO Directivos VALUES (33,'INFORME 1');
 INSERT INTO Directivos VALUES(15, 'INFORME 1');
@@ -237,36 +241,28 @@ INSERT INTO Director_marketing VALUES (77, 'control1');
 INSERT INTO Director_marketing VALUES (17, 'control1');
 INSERT INTO Director_marketing VALUES (14, 'control1');
 INSERT INTO Director_marketing VALUES (44, 'control1');
---DEPARTAMENTO
-INSERT INTO Departamento VALUES (1,1);
-INSERT INTO Departamento VALUES (2,2);
-INSERT INTO Departamento VALUES (3,3);
-INSERT INTO Departamento VALUES (4,4);
+
 
 --ILUMINACION
-INSERT INTO Iluminacion VALUES(1, 'LAMPARAS','SMART');
+INSERT INTO Iluminacion VALUES(11,1, 'Led','SMART');
 
 --JARDINERIA
-INSERT  INTO Jardineria VALUES(2, 'MACETAS','PLANTAS');
+INSERT  INTO Jardineria VALUES(22,2, 'Grande','PLANTAS');
 
 --MUEBLES
-INSERT INTO Muebles VALUES (3, 'TIPO','Madera');
+INSERT INTO Muebles VALUES (33,3, 'TIPO','Madera');
 
 --BRICOLAJE
-INSERT INTO Bricolaje VALUES (4 , 'INERIOR','HERRAMIENTAS');
+INSERT INTO Bricolaje VALUES (44,4 , 'INERIOR','Martillo');
 --VENDEN
-INSERT INTO Venden VALUES(1,1);
-INSERT INTO Venden VALUES(2,2);
-INSERT INTO Venden VALUES (3,3);
+INSERT INTO Venden VALUES(11,1,1);
+INSERT INTO Venden VALUES(22,2,2);
+INSERT INTO Venden VALUES(33,3,3);
 --LINEA DE PEDIDO
 INSERT INTO Linea_de_pedido VALUES(1,1,'Pedido de focos para evento');
 INSERT INTO Linea_de_pedido VALUES (2,2,'Pedido de tijeras podadoras');
 INSERT INTO Linea_de_pedido VALUES (3,3,'Pedido de escritorios de 12 piezas');
 --PEDIDOS
-INSERT INTO Pedidos VALUES (1,'YYYY-MM-DD',1,'76665434D');
-INSERT INTO Pedidos VALUES (2,'YYYY-MM-DD',2,'76665434D');
-INSERT INTO Pedidos VALUES (3,'YYYY-MM-DD',3,'77889767D');
-
-
-
-
+INSERT INTO Pedidos VALUES (1,'02/02/2022',1,'76665434D');
+INSERT INTO Pedidos VALUES (2,'30/05/2022',2,'76665434D');
+INSERT INTO Pedidos VALUES (3,'16/04/2022',3,'77889767D');
